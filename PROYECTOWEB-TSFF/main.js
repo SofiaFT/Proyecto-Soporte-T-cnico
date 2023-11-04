b594cdcca732649c02be741107c87bfb2274ffd2
Sofia F <149523143+SofiaFT@users.noreply.github.com>
2023-11-03 21:53:35 -0600

    Add files via upload

---
+++ PROYECTOWEB-TSFF/main.js
@@ -0,0 +1,10 @@
+//funcion que se ejecutara cuando hago click en el boton
+function muestra_oculta(id) {
+    let div = document.getElementById(id);
+    if (div.style.display == "none"){ 
+      div.style.display = "flex";
+    } 
+    else {
+      div.style.display = "none";
+    }
+}
